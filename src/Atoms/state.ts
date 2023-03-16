d30ca1914da71e5a454d69fc5c5413dbc4940d80
선채훈 <sunchaehoon@naver.com>
2023-03-16 16:49:48 +0900

    atoms

--- src/Atoms/state.ts
+++ src/Atoms/state.ts
@@ -1,10 +1,27 @@
 import { atom } from "recoil";
+import { FaceProps } from "@/types/analyzers";
 
-const PreviewAtom = atom<string>({
+const imgBaseAtom = atom<string>({
     key: 'imgbase',
     default: "",
+});
+
+const faceInfoAtom = atom<FaceProps>({
+    key: "faceInfo",
+    default: {
+        gender: {
+            value: "",
+            confidence: 0,
+        },
+        age: {
+            value: "",
+            confidence: 0,
+        },
+        emotion: {
+            value: "",
+            confidence: 0,
+        },
+    },
 })
 
-
-
-export { PreviewAtom };
+export { imgBaseAtom, faceInfoAtom };
