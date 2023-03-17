734f23640f5c349b3da4cf4d2ceaab6e43958add
선채훈 <sunchaehoon@naver.com>
2023-03-17 14:57:36 +0900

    analyzed

--- src/pages/analyzed.tsx
+++ src/pages/analyzed.tsx
@@ -1,37 +1,112 @@
-import { faceInfoAtom, imgBaseAtom } from '@/Atoms/state';
+import { celebrityAtom, faceInfoAtom, imgBaseAtom } from '@/Atoms/state';
 import { Header } from '@/components';
 import styled from '@emotion/styled';
 import next from 'next';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { useRecoilState } from 'recoil';
 
 const analyzed = () => {
   const [imgBase, setImgBase] = useRecoilState(imgBaseAtom);
   const [faceInfo, setFaceInfo] = useRecoilState(faceInfoAtom);
+  const [celebrity, setCelebrity] = useRecoilState(celebrityAtom);
   const Usepercentage = (n: number) => (n * 100).toFixed(1);
+  const router = useRouter();
 
   return (
     <>
       <Header />
       <Wrapper>
-        <ImgWrapper>
-          <img src={imgBase} alt="선택한 이미지" />
-        </ImgWrapper>
+        <LeftCOntentWrapper>
+          <ImgWrapper>
+            <Image
+              src={imgBase}
+              layout="fill"
+              objectFit="cover"
+              alt="이미지 사진"
+            />
+          </ImgWrapper>
+          <ReChooseBtn
+            onClick={() => {
+              router.push('/');
+              setImgBase('');
+            }}
+          >
+            처음으로
+          </ReChooseBtn>
+        </LeftCOntentWrapper>
+
         <ContentWrapper>
           <Content>
-            {`성별 : ${faceInfo.gender.value === "male" ? "남자" : "여자"} (${Usepercentage(faceInfo.gender.confidence)})%`}
+            {`성별 : ${
+              faceInfo.gender.value === 'male' ? '남자' : '여자'
+            } (${Usepercentage(faceInfo.gender.confidence)})%`}
           </Content>
           <Content>
-            {`나이 : ${faceInfo.age.value} (${Usepercentage(faceInfo.age.confidence)})%`}
+            {`나이 : ${faceInfo.age.value} (${Usepercentage(
+              faceInfo.age.confidence
+            )})%`}
           </Content>
           <Content>
-            {`표정 : ${faceInfo.emotion.value} (${Usepercentage(faceInfo.emotion.confidence)})%`}
+            {`표정 : ${
+              faceInfo.emotion.value === 'angry'
+                ? '화난 표정'
+                : faceInfo.emotion.value === 'disgust'
+                ? '싫은 표정'
+                : faceInfo.emotion.value === 'fear'
+                ? '두려운 표정'
+                : faceInfo.emotion.value === 'laugh'
+                ? '웃는 표정'
+                : faceInfo.emotion.value === 'neutral'
+                ? '무표정'
+                : faceInfo.emotion.value === 'sad'
+                ? '슬픈 표정'
+                : faceInfo.emotion.value === 'surprise'
+                ? '놀란 표정'
+                : faceInfo.emotion.value === 'smile'
+                ? '웃는 표정'
+                : faceInfo.emotion.value === 'talking'
+                ? '대화중인 표정'
+                : '___'
+            } (${Usepercentage(faceInfo.emotion.confidence)})%`}
+          </Content>
+          <Content>
+            {`닮은 연예인 : ${celebrity.celebrity.value} (${Usepercentage(
+              celebrity.celebrity.confidence
+            )})%`}
           </Content>
         </ContentWrapper>
       </Wrapper>
     </>
   );
 };
+
+const LeftCOntentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 60px;
+  width: 25%;
+`;
+
+const ReChooseBtn = styled.button`
+  width: 40%;
+  height: 50px;
+  cursor: pointer;
+  font-size: 1.3rem;
+  border: none;
+  border-radius: 10px;
+  box-shadow: rgba(205, 205, 205, 0.2) 0px 8px 24px;
+  transition: all ease-in-out 0.3s;
+  background-color: #00adb5;
+  color: white;
+
+  :hover {
+    background-color: #008a90;
+    transition-duration: 0.15s;
+  }
+`;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -41,7 +116,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 40px;
+  gap: 100px;
 `;
 
 const ImgWrapper = styled.div`
@@ -51,7 +126,9 @@ const ImgWrapper = styled.div`
   align-items: center;
   border-radius: 10px;
   box-shadow: rgba(255, 255, 255, 0.2) 0px 8px 24px;
-  margin-top: -50px;
+  overflow: hidden;
+  width: 100%;
+  padding-bottom: 100%;
 `;
 
 const ContentWrapper = styled.div`
@@ -60,6 +137,7 @@ const ContentWrapper = styled.div`
   justify-content: center;
   align-items: center;
   gap: 20px;
+  margin-top: -35px;
 `;
 
 const Content = styled.div`
