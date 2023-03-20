feafd1cd9f5824aab2936bca27c22ea90e86b9c0
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2023-03-20 23:17:15 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/openapi/:path*',
+        destination: `https://openapi.naver.com/:path*`,
+      },
+    ];
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
