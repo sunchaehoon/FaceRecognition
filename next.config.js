94f9dc4bd14a99a02827479ddbfdcdd29b09524e
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2023-03-21 12:23:27 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
+        source: '/openapi/:path*',
         destination: `https://openapi.naver.com/:path*`,
       },
     ];
